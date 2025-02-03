9081c33bc505c1018a07a1a47ab338d2212c63ed
Chanui <jcu011@naver.com>
2025-02-03 20:13:22 +0900

    feat: 전체 회원 조회 API 추가(임시로 추가한 API, 친구 서비스 생성시 삭제 예정)

--- src/main/java/chatflow/memberservice/MemberServiceApplication.java
+++ src/main/java/chatflow/memberservice/MemberServiceApplication.java
@@ -1,24 +1,24 @@
 package chatflow.memberservice;
 
-import com.netflix.discovery.EurekaClient;
+//import com.netflix.discovery.EurekaClient;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @RequiredArgsConstructor
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MemberServiceApplication {
-    private final EurekaClient eurekaClient;
+//    private final EurekaClient eurekaClient;
 
     public static void main(String[] args) {
         SpringApplication.run(MemberServiceApplication.class, args);
     }
 
-    @PreDestroy
-    public void unregister() {
-        eurekaClient.shutdown();
-    }
+//    @PreDestroy
+//    public void unregister() {
+//        eurekaClient.shutdown();
+//    }
 }
--- src/main/java/chatflow/memberservice/controller/MemberController.java
+++ src/main/java/chatflow/memberservice/controller/MemberController.java
@@ -3,6 +3,7 @@ package chatflow.memberservice.controller;
 import chatflow.memberservice.dto.ApiResponse;
 import chatflow.memberservice.dto.member.request.MemberUpdateRequest;
 import chatflow.memberservice.security.MemberAuthorize;
+import chatflow.memberservice.service.AdminService;
 import chatflow.memberservice.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,6 +22,7 @@ import java.util.UUID;
 @RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
+    private final AdminService adminService;
 
     @Operation(summary = "회원 정보 조회")
     @GetMapping
@@ -38,5 +40,11 @@ public class MemberController {
     @PutMapping
     public ApiResponse updateMember(@AuthenticationPrincipal User user, @Valid @RequestBody MemberUpdateRequest request) {
         return ApiResponse.success(memberService.updateMember(UUID.fromString(user.getUsername()), request));
+    }
+
+    @Operation(summary = "회원 전체 목록 조회 (친구 서비스 생성 전 임시 API)")
+    @GetMapping("/friends")
+    public ApiResponse getAllMembers() {
+        return ApiResponse.success(adminService.getMembers());
     }
 }
