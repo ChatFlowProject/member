9621e1653fe01a22be56b4896692e3cae3e24d4b
Chanui <jcu011@naver.com>
2025-02-11 21:53:57 +0900

    feat: 1.2.2 OPTIONS 메서드 허용

--- src/main/java/chatflow/memberservice/security/SecurityConfig.java
+++ src/main/java/chatflow/memberservice/security/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableMethodSecurity
 @RequiredArgsConstructor
@@ -21,15 +26,32 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationEntryPoint entryPoint;
 
-    private static final String[] ALLOWED_URIS = {"/sign-up", "/sign-in"};
+    private static final String[] ALLOWED_PATH = {"/sign-up", "/sign-in"};
+    private static final List<String> ALLOWED_ORIGIN = List.of("http://localhost:3000");
+    private static final List<String> ALLOWED_ORIGIN_PATTERN = List.of("http://*.flowchat.shop", "https://*.flowchat.shop");
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ALLOWED_ORIGIN); // 허용할 Origin 설정
+        configuration.setAllowedOriginPatterns(ALLOWED_ORIGIN_PATTERN); // 허용할 Origin 패턴 설정
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 지정
+        configuration.setAllowedHeaders(List.of("X-Requested-With", "Origin", "Accept", "Content-Type", "Authorization", "X-XSRF-token")); // 허용할 헤더 설정
+        configuration.setAllowCredentials(true); // 자격 증명(쿠키 등) 허용 여부 설정
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(CsrfConfigurer<HttpSecurity>::disable)
                 .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers(ALLOWED_URIS).permitAll()
+                        request.requestMatchers(ALLOWED_PATH).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
