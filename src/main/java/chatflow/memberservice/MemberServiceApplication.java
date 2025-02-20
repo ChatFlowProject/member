ab2ebc8c3f5a5c235f3541dbd65ae7db851de781
Chanui <jcu011@naver.com>
2025-02-20 21:59:59 +0900

    chore: 안쓰는 패키지 정리

--- src/main/java/chatflow/memberservice/MemberServiceApplication.java
+++ src/main/java/chatflow/memberservice/MemberServiceApplication.java
@@ -1,24 +1,15 @@
 package chatflow.memberservice;
 
-//import com.netflix.discovery.EurekaClient;
-import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @RequiredArgsConstructor
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class MemberServiceApplication {
-//    private final EurekaClient eurekaClient;
 
     public static void main(String[] args) {
         SpringApplication.run(MemberServiceApplication.class, args);
     }
 
-//    @PreDestroy
-//    public void unregister() {
-//        eurekaClient.shutdown();
-//    }
 }
