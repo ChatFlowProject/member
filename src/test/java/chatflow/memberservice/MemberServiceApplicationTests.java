bd3411c9fe1252a62b4123ab5cb955af76eb4429
Chanui <jcu011@naver.com>
2025-04-07 06:02:41 +0900

    chore: 설정정보 변경 - image build시 ApplicationTests에서 BeanCreationException 오류 수정

--- src/test/java/chatflow/memberservice/MemberServiceApplicationTests.java
+++ src/test/java/chatflow/memberservice/MemberServiceApplicationTests.java
@@ -2,8 +2,10 @@ package chatflow.memberservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class MemberServiceApplicationTests {
 
 	@Test
