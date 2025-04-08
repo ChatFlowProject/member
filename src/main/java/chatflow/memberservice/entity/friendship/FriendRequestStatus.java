443ef513aa603db04d0c4939271ce28078d104a6
Chanui <jcu011@naver.com>
2025-04-09 03:15:10 +0900

    feat: 친구 관계 상태 enum 추가 (친구 요청 전용 enum) INVALID_REQUEST, ALREADY_FRIENDS, REQUEST_SUCCESS, FRIENDSHIP_ESTABLISHED

---
+++ src/main/java/chatflow/memberservice/entity/friendship/FriendRequestStatus.java
@@ -0,0 +1,8 @@
+package chatflow.memberservice.entity.friendship;
+
+public enum FriendRequestStatus {
+    INVALID_REQUEST,
+    ALREADY_FRIENDS,
+    REQUEST_SUCCESS,
+    FRIENDSHIP_ESTABLISHED
+}
