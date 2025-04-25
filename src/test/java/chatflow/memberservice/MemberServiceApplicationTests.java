3795cde554eac7bc04270305a0618d8bd0a8d8d8
Chanui <jcu011@naver.com>
2025-04-26 00:40:14 +0900

    test: 테스트 클래스 EmbeddedKafka 추가

--- src/test/java/chatflow/memberservice/MemberServiceApplicationTests.java
+++ src/test/java/chatflow/memberservice/MemberServiceApplicationTests.java
@@ -2,8 +2,10 @@ package chatflow.memberservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @SpringBootTest
 @ActiveProfiles("dev")
 class MemberServiceApplicationTests {
