45f82144561bed28484fdbae0c5918ba91a23824
Chanui <jcu011@naver.com>
2025-05-20 11:53:07 +0900

    fix: @EnableScheduling 추가

--- src/main/java/chatflow/memberservice/MemberServiceApplication.java
+++ src/main/java/chatflow/memberservice/MemberServiceApplication.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @RequiredArgsConstructor
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class MemberServiceApplication {
 
